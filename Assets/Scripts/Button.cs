a3bf01cba7e0a33b9ee33e6cc4f16d7803b07cc8
Olga Kopylova <olenkakopylova@gmail.com>
2018-03-22 07:59:39 +0300

    Initial commit

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour {
+
+	public void OnClick(){
+		if(Input.GetButtonDown("Play")){
+			print ("start");
+			SceneManager.LoadScene ("level1");
+		}
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public Vector2 speed = new Vector2 (50, 50);
+	private Vector2 movement;
+	public Rigidbody2D rb2D;
+
+	void Start() {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate(){
+		rb2D.MovePosition(rb2D.position + movement);
+	}
+}
